1fb127b6fc03f367d468e6eaae61b57900bafe83
Persis <pboateng@soka.edu>
2025-06-23 09:21:00 -0700

    Update editor.c

--- editor.c
+++ editor.c
@@ -35,6 +35,14 @@ void enableRawMode(){
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
 
 int main(){
   
